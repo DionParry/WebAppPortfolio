30f7ef373277134718701177ea36b089bf10f0ab
Dion John Parry <dionjohnparry@gmail.com>
2021-07-10 19:07:59 +0100

    Missed on previous commit.

--- WebPortfolio/WebPortfolio/Models/ViewModels/ResumeViewModel.cs
+++ WebPortfolio/WebPortfolio/Models/ViewModels/ResumeViewModel.cs
@@ -5,12 +5,19 @@ namespace WebPortfolio.Models.ViewModels
     public class ResumeViewModel
     {
         public string Title { get; set; }
+        public string ApplicantName { get; set; }
+        public string ProfileDescription { get; set; }
+        public int Test { get; set; }
 
         public static async Task<ResumeViewModel> LoadFrom()
         {
             var vm = new ResumeViewModel
             {
-                Title = "Software Developer"
+                Title = "Software Developer",
+                ApplicantName = "DionParry",
+                ProfileDescription = "A confident and highly motivated individual with strong problem solving abilities." +
+                                     "Skilled at working closely with others in a team environment." +
+                                     "Looking for a change of pace to expand my knowledge in software development."
             };
 
             return vm;
