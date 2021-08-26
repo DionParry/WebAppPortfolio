fa7135ab67dbf9e046444fd0ef17d400260b2040
Dion Parry <dionjohnparry@gmail.com>
2021-08-26 23:06:31 +0100

    Test project added with initial tests
    * having slight issue with package not building during test, need to investigate why.

---
+++ WebPortfolio/Tests/UnitTests/ApplicationServiceTests/HomeApplicationTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using WebPortfolio.ApplicationServices;
+using WebPortfolio.ApplicationServices.Interfaces;
+using WebPortfolio.Models.ViewModels;
+
+namespace Tests.UnitTests.ApplicationServiceTests
+{
+    public class HomeApplicationTest
+    {
+        [Test]
+        public async Task GetResumeViewModel_WhenCalled_ReturnsViewModel()
+        {
+            // Arrange
+            var appService = CreateApplicationService();
+            // Act
+            var viewModel = await appService.GetResumeViewModel();
+            // Assert
+            Assert.IsInstanceOf<ResumeViewModel>(viewModel);
+        }
+        
+        private static IHomeApplicationService CreateApplicationService()
+        {
+            return new HomeApplicationService();
+        }
+    }
+}
---
+++ WebPortfolio/Tests/UnitTests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Moq;
+using NUnit.Framework;
+using WebPortfolio.ApplicationServices.Interfaces;
+using WebPortfolio.Controllers;
+using WebPortfolio.Models.ViewModels;
+
+namespace Tests.UnitTests.ControllerTests
+{
+    public class HomeControllerTest
+    {
+        [Test]
+        public async Task Resume_WhenExecuted_ReturnsViewModel()
+        {
+            //Arrange
+            var fakeAppService = new Mock<IHomeApplicationService>();
+            fakeAppService.Setup(x => x.GetResumeViewModel());
+            var homeController = CreateController(fakeAppService.Object);
+            
+            //Act
+            var result = await homeController.Resume() as ViewResult;
+            
+            //Assert
+            Assert.IsInstanceOf<ResumeViewModel>(result?.Model);
+        }
+        
+        private static HomeController CreateController(
+             IHomeApplicationService homeApplicationService = null)
+        {
+            var fakeHomeApplicationService = homeApplicationService ?? new Mock<IHomeApplicationService>().Object;
+            return new HomeController(fakeHomeApplicationService);
+        }
+    }
+}
