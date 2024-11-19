0d57bdec35e7b4aa0018cdaec528e381ef460e4c
Dion Parry <dionjohnparry@gmail.com>
2024-11-19 22:04:13 +0000

    Updated cv 19/11/2024

--- WebPortfolio/WebPortfolio/Models/ViewModels/ResumeViewModel.cs
+++ WebPortfolio/WebPortfolio/Models/ViewModels/ResumeViewModel.cs
@@ -9,6 +9,7 @@ namespace WebPortfolio.Models.ViewModels
         public string ApplicantName { get; set; }
         public string ProfileDescription { get; set; }
         public string ProjectDescription { get; set; }
+        public string ProjectDescription2 { get; set; }
         public IEnumerable<string> TechnicalSkills { get; set; }
         public IEnumerable<string> References { get; set; }
         public IEnumerable<string> Achievements { get; set; }
@@ -17,10 +18,12 @@ namespace WebPortfolio.Models.ViewModels
         public Dictionary<string, List<string>> EmploymentHistoryDescription { get; set; }
         public string JobTitle1 { get; set; }
         public string JobTitle2 { get; set; }
+        public string JobTitle3 { get; set; }
         public ResumeViewModel LoadFrom()
         {
             const string jobTitle1 = "Analyst Programmer";
             const string jobTitle2 = "Software Developer";
+            const string jobTitle3 = "Software Team Lead";
 
             var jobDescription1 = new List<string>
             {
@@ -34,27 +37,37 @@ namespace WebPortfolio.Models.ViewModels
                 "towards new innovative systems",
                 "Writing scalable, maintainable and testable code to a high standard using C# .NET " +
                 "whilst Maintaining existing applications and products (including legacy technologies).",
+            };
+            var jobDescription3 = new List<string>
+            {
+                "Introduced and modernised the work processes, practices and development / Qa team as a whole",
+                "Key role in planning the migration of tech debt and older work streams to newer standards.",
+                // "Writing scalable, maintainable and testable code to a high standard using C# .net with an eye on ensuring the developers within the team are meeting the standard set.",
+                "Actively engaged in bi-monthly lead developer meetings to organise and address integration / problems within the different software teams.",
                 "Responsible for overseeing / delivering weekly deployments ensuring pull requests are to a high coding " +
-                "standard and adhere to good coding conventions/ best practices."
+                "standard and adhere to good coding conventions/ best practices.",
+                "Actively supporting and suggesting security improvements through azure and inbuilt methods to better secure the company.",
+                "Regularly performing reviews on an individual and team basis where appropriate.",
+                "Responsible for overseeing the recruitment of developers within the team. "
             };
 
             var vm = new ResumeViewModel
             {
-                Title = "Software Developer",
+                Title = "Software Team Lead",
                 ApplicantName = "DionParry",
                 ProfileDescription = "A confident and highly motivated individual with strong problem solving abilities. " +
                                      "Skilled at working closely with others in a team environment. " +
-                                     "Looking to utilize / explore newer technologies to expand my knowledge in software development.",
+                                     "Looking for a role to put my skills and knowledge into practice by improving team performance, organisation productivity and application quality",
                 ProjectDescription = "Using Unity and C# to develop interactive interfaces, adaptive environments with " +
-                                      "use of artificial intelligence and movement mechanics whilst adhering to VR best practices.",
+                                     "use of artificial intelligence and movement mechanics whilst adhering to VR best practices.",
+                ProjectDescription2 = "At the later stages of releasing a mobile app to meet a gap within the market using expo, react native and .net",
                 TechnicalSkills = new List<string>
                 {
-                    "C# .Net Framework / MVC / .Net 6",
-                    "Entity Framework / Entity Core",
-                    "React / Redux / Typescript",
+                    "C# .Net",
+                    "React / Redux / React Query / Typescript",
                     "DDD, TDD, SOLID PRINCIPLES",
-                    "Git / TeamCity / Octopus",
-                    "Unity Game Engine (C#) / Blender / MAYA",
+                    "Git Actions / Team City / Octopus",
+                    "Azure"
                 },
                 References = new List<string> {
                     "Dr Llyr Ap Cenydd (Developer of Ocean Rift)",
@@ -62,18 +75,19 @@ namespace WebPortfolio.Models.ViewModels
                 },
                 ContactDetails = new List<string>
                 {
-                    "GitLab", "GitHub", "Mobile", "Email", "Address"
+                    "GitHub", "Mobile", "Email", "Address"
                 },
                 ContactDetailsDescription = new List<string> {
-                    "https://gitlab.com/DionParry", "https://github.com/DionParry",
-                    "07592 757 905", "dionjohnparry@gmail.com", "Ellesmere Port, Cheshire"
+                    "https://github.com/DionParry", "07592 757 905", "dionjohnparry@gmail.com", "Ellesmere Port, Cheshire"
                 },
                 JobTitle1 = jobTitle1,
                 JobTitle2 = jobTitle2,
+                JobTitle3 = jobTitle3,
                 EmploymentHistoryDescription = new Dictionary<string, List<string>>
                 {
                     {jobTitle1, jobDescription1},
                     {jobTitle2, jobDescription2},
+                    {jobTitle3, jobDescription3},
                 },
                 Achievements =  new List<string>
                 {
